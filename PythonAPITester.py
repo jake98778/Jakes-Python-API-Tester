9b3db27cd5a3b437a1e9f84c4bbfe3f1ea6dbd69
jake98778 <jake98778@gmail.com>
2022-10-06 22:38:00 -0400

    Update PythonAPITester.py

--- PythonAPITester.py
+++ PythonAPITester.py
@@ -7,24 +7,34 @@ ws.title("Jakes API Tester")
 ws.geometry('400x300')
 ws['bg'] = '#4242f5'
 
+#I'm an artist so this is where my art skills
 def printValue():
     URL = api_url.get()
-    Label(ws, text=f'{URL}, Has been saved as API URL', pady=20, bg='#4242f5').pack()
+    Label(ws, text=f'"{URL}" Has been saved as API URL', pady=20, bg='#4242f5').pack()
 
-    response = requests.get(URL)
-
+def printApi():
+    response = requests.get(api_url)
     Label(ws, text=f'Response:  {response}', pady=20, bg='#4242f5').pack()
-
 
 api_url = Entry(ws)
 api_url.pack(pady=30)
 
+#First Button to save the API URL so we have it as a easy to mess with var
 Button(
     ws,
-    text="Register Player", 
+    text="Enter URL to API test with", 
     padx=10, 
     pady=5,
     command=printValue
+    ).pack()
+
+#another button to send the API
+Button(
+    ws,
+    text="Send da API", 
+    padx=20, 
+    pady=10,
+    command=printApi
     ).pack()
 
 ws.mainloop()
